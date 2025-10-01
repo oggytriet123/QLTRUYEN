c968fffd21daabc37e1dd4f355a52fe83349f89c
trung <hatrongtrungpro@gmail.com>
2025-10-01 21:57:40 +0700

    update

--- QLTV.cpp
+++ QLTV.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 // Khai báo c?u trúc d? li?u Truy?n (mã, tên, tác gi?, th? lo?i, t?p s?, don giá thuê/ngày, tình tr?ng)
 typedef struct {
 	char ma_truyen[20];
@@ -9,10 +10,21 @@ typedef struct {
 	char the_loai[20];
 	int tap_so[10];
 	float don_gia_thue;
-	int tinh_trang; // 1 la con; 0 la da cho thue	
+	bool tinh_trang; // 1 la con; 0 la da cho thue
 } truyen;
+
+struct Node{
+    truyen data;
+    struct Node *pNext;
+}Node;
+
+struct Stack{
+    Node *top;
+};
+void Push(Stack *t,truyen x)
+
 int main()
 {
-	
+
 }
 
