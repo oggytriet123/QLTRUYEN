8a458cb3ed1a9ff52853658b6a11bce4b41ee93e
Tuan Anh <tuananh7088@gmail.com>
2025-10-01 16:30:34 +0700

    add Struct Truyen

--- QLTV.cpp
+++ QLTV.cpp
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+// Khai báo c?u trúc d? li?u Truy?n (mã, tên, tác gi?, th? lo?i, t?p s?, don giá thuê/ngày, tình tr?ng)
+typedef struct {
+	char ma_truyen[20];
+	char ten[20];
+	char tac_gia[20];
+	char the_loai[20];
+	int tap_so[10];
+	float don_gia_thue;
+	int tinh_trang; // 1 la con; 0 la da cho thue	
+} truyen;
 int main()
 {
+	
 }
 
